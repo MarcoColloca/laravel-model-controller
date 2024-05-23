5e075a26e018b2b4a8422227df2f61dadbfeded2
MarcoColloca <marco.colloca3511@gmail.com>
2024-05-23 15:54:06 +0200

    Feat → generated movies' cards in welcome.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,31 @@
 @section('main-content')
     <section class="mt-5 py-1">
         <div class="container bg-dark py-4">
-            <h1 class="title text-center text-success">Welcome!</h1>
+            <h1 class="title text-center text-success">Movies!</h1>
         </div>
     </section>
 
 
     <section class="mb-5 py-1">
         <div class="bg-light container py-4">
-            <p class="title text-center text-dark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni totam expedita quo hic at ratione fuga asperiores nemo quae aspernatur rerum, aut repellat. Molestiae dolores ad est, maiores at provident velit dolor, illum amet harum quo quibusdam doloribus quisquam ut quasi? Non repudiandae cum doloribus aperiam minus, porro dolores suscipit saepe odit voluptas ad culpa alias dolorem repellat accusantium excepturi, beatae ipsum fuga nobis quidem quos repellendus exercitationem iure? Id asperiores nam commodi facilis nihil magni fugiat atque esse delectus officia, minus sapiente accusantium, quos quibusdam optio, ut incidunt eveniet expedita totam aspernatur nobis ratione dolor. Sequi molestias tempore doloribus?</p>
+            <div class="row g-5">
+                @foreach ($movies as $movie)
+                        
+                    <div class="col-4">
+                        <div class="card">
+                            <div class="card-header my-movie-card-header">
+                                <h4 class="text-center">{{$movie->title}}</h4>
+                            </div>
+                            <div class="card-body">                                
+                                <p>Original Title: {{$movie->original_title}}</p>
+                                <p>Nationality: {{$movie->nationality}}</p>
+                                <p>Realese Date: {{$movie->date}}</p>
+                                <p>Vote: {{$movie->vote}}</p>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
         </div>
     </section>    
 @endsection
